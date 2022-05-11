aa4d601f5c89258f557de743ea855f1e5f1f1cb7
Mykola Lehkyi <mykola.lehkyi.dev@gmail.com>
2022-05-11 19:39:42 +0300

    Chapter2

---
+++ src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardService } from "./dashboard.service";
+import { Dashboard } from "./dashboard";
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiHost: 'https://your-server-host/api/v1',
+};
